ab75ff3f4fb08c31c705c7957499b58cd6eddc23
Joao-Leite-ads <joaoguilhermeleite02@gmail.com>
2026-09-19 16:17:19 -0300

    .

--- bingo.c
+++ bingo.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define LINHAS  5
+#define LINHAS 5
 #define COLUNAS 5
-
-typedef enum {
+typedef enum
+{
     B = 0,
     I,
     N,
@@ -13,39 +13,49 @@ typedef enum {
     O
 } Coluna;
 
-typedef struct {
+typedef struct
+{
     int numeros[LINHAS][COLUNAS];
 } Cartela;
 
-const char *nomesColunas[COLUNAS] = { "B", "I", "N", "G", "O" };
-const int faixaMinima[COLUNAS]    = { 1, 16, 31, 46, 61 };
-const int faixaMaxima[COLUNAS]    = { 15, 30, 45, 60, 75 };
+const char *nomesColunas[COLUNAS] = {"B", "I", "N", "G", "O"};
+const int faixaMinima[COLUNAS] = {1, 16, 31, 46, 61};
+const int faixaMaxima[COLUNAS] = {15, 30, 45, 60, 75};
 
-int  gerarNumero(int minimo, int maximo);
-int  numeroExiste(int valores[], int quantidade, int numero);
+/* Protótipos das funções */
+int gerarNumero(int minimo, int maximo);
+int numeroExiste(int valores[], int quantidade, int numero);
 void ordenarColuna(int valores[], int quantidade);
 void gerarCartela(Cartela *cartela);
 void imprimirCartela(const Cartela *cartela, int numeroDaCartela);
-int  cartelasIguais(const Cartela *c1, const Cartela *c2);
-int  cartelaDuplicada(const Cartela cartelas[], int quantidadeAtual, const Cartela *nova);
+int cartelasIguais(const Cartela *c1, const Cartela *c2);
+int cartelaDuplicada(const Cartela cartelas[], int quantidadeAtual, const Cartela *nova);
 
-int gerarNumero(int minimo, int maximo) {
+int gerarNumero(int minimo, int maximo)
+{
     return minimo + rand() % (maximo - minimo + 1);
 }
 
-int numeroExiste(int valores[], int quantidade, int numero) {
-    for (int i = 0; i < quantidade; i++) {
-        if (valores[i] == numero) {
+int numeroExiste(int valores[], int quantidade, int numero)
+{
+    for (int i = 0; i < quantidade; i++)
+    {
+        if (valores[i] == numero)
+        {
             return 1;
         }
     }
     return 0;
 }
 
-void ordenarColuna(int valores[], int quantidade) {
-    for (int i = 0; i < quantidade - 1; i++) {
-        for (int j = 0; j < quantidade - 1 - i; j++) {
-            if (valores[j] > valores[j + 1]) {
+void ordenarColuna(int valores[], int quantidade)
+{
+    for (int i = 0; i < quantidade - 1; i++)
+    {
+        for (int j = 0; j < quantidade - 1 - i; j++)
+        {
+            if (valores[j] > valores[j + 1])
+            {
                 int aux = valores[j];
                 valores[j] = valores[j + 1];
                 valores[j + 1] = aux;
@@ -54,18 +64,23 @@ void ordenarColuna(int valores[], int quantidade) {
     }
 }
 
-void gerarCartela(Cartela *cartela) {
-    for (int col = 0; col < COLUNAS; col++) {
+void gerarCartela(Cartela *cartela)
+{
+    for (int col = 0; col < COLUNAS; col++)
+    {
         int valores[LINHAS];
         int quantidadeGerada = 0;
 
-        for (int lin = 0; lin < LINHAS; lin++) {
-            if (lin == 2 && col == 2) {
+        for (int lin = 0; lin < LINHAS; lin++)
+        {
+            if (lin == 2 && col == 2)
+            {
                 continue;
             }
 
             int numero;
-            do {
+            do
+            {
                 numero = gerarNumero(faixaMinima[col], faixaMaxima[col]);
             } while (numeroExiste(valores, quantidadeGerada, numero));
 
@@ -76,10 +91,14 @@ void gerarCartela(Cartela *cartela) {
         ordenarColuna(valores, quantidadeGerada);
 
         int idx = 0;
-        for (int lin = 0; lin < LINHAS; lin++) {
-            if (lin == 2 && col == 2) {
+        for (int lin = 0; lin < LINHAS; lin++)
+        {
+            if (lin == 2 && col == 2)
+            {
                 cartela->numeros[lin][col] = 0;
-            } else {
+            }
+            else
+            {
                 cartela->numeros[lin][col] = valores[idx];
                 idx++;
             }
@@ -87,19 +106,26 @@ void gerarCartela(Cartela *cartela) {
     }
 }
 
-void imprimirCartela(const Cartela *cartela, int numeroDaCartela) {
+void imprimirCartela(const Cartela *cartela, int numeroDaCartela)
+{
     printf("Cartela %d:\n", numeroDaCartela);
 
-    for (int col = 0; col < COLUNAS; col++) {
+    for (int col = 0; col < COLUNAS; col++)
+    {
         printf(" %-3s", nomesColunas[col]);
     }
     printf("\n");
 
-    for (int lin = 0; lin < LINHAS; lin++) {
-        for (int col = 0; col < COLUNAS; col++) {
-            if (lin == 2 && col == 2) {
+    for (int lin = 0; lin < LINHAS; lin++)
+    {
+        for (int col = 0; col < COLUNAS; col++)
+        {
+            if (lin == 2 && col == 2)
+            {
                 printf(" %3s", "X");
-            } else {
+            }
+            else
+            {
                 printf(" %3d", cartela->numeros[lin][col]);
             }
         }
@@ -107,10 +133,15 @@ void imprimirCartela(const Cartela *cartela, int numeroDaCartela) {
     }
     printf("\n");
 }
-int cartelasIguais(const Cartela *c1, const Cartela *c2) {
-    for (int lin = 0; lin < LINHAS; lin++) {
-        for (int col = 0; col < COLUNAS; col++) {
-            if (c1->numeros[lin][col] != c2->numeros[lin][col]) {
+
+int cartelasIguais(const Cartela *c1, const Cartela *c2)
+{
+    for (int lin = 0; lin < LINHAS; lin++)
+    {
+        for (int col = 0; col < COLUNAS; col++)
+        {
+            if (c1->numeros[lin][col] != c2->numeros[lin][col])
+            {
                 return 0;
             }
         }
@@ -118,37 +149,46 @@ int cartelasIguais(const Cartela *c1, const Cartela *c2) {
     return 1;
 }
 
-int cartelaDuplicada(const Cartela cartelas[], int quantidadeAtual, const Cartela *nova) {
-    for (int i = 0; i < quantidadeAtual; i++) {
-        if (cartelasIguais(&cartelas[i], nova)) {
+int cartelaDuplicada(const Cartela cartelas[], int quantidadeAtual, const Cartela *nova)
+{
+    for (int i = 0; i < quantidadeAtual; i++)
+    {
+        if (cartelasIguais(&cartelas[i], nova))
+        {
             return 1;
         }
     }
     return 0;
 }
 
-int main(void) {
-    srand((unsigned int) time(NULL));
+int main(void)
+{
+    srand((unsigned int)time(NULL));
 
     int quantidade;
 
     printf("=== Gerador de Cartelas de Bingo ===\n\n");
     printf("Quantas cartelas deseja gerar? ");
 
-    if (scanf("%d", &quantidade) != 1 || quantidade <= 0) {
+    if (scanf("%d", &quantidade) != 1 || quantidade <= 0)
+    {
         printf("Quantidade invalida.\n");
         return 1;
     }
-    Cartela *cartelas = (Cartela *) malloc((size_t) quantidade * sizeof(Cartela));
-    if (cartelas == NULL) {
+
+    Cartela *cartelas = (Cartela *)malloc((size_t)quantidade * sizeof(Cartela));
+    if (cartelas == NULL)
+    {
         printf("Erro ao alocar memoria.\n");
         return 1;
     }
 
-    for (int i = 0; i < quantidade; i++) {
+    for (int i = 0; i < quantidade; i++)
+    {
         Cartela nova;
 
-        do {
+        do
+        {
             gerarCartela(&nova);
         } while (cartelaDuplicada(cartelas, i, &nova));
 
@@ -156,7 +196,8 @@ int main(void) {
     }
 
     printf("\n");
-    for (int i = 0; i < quantidade; i++) {
+    for (int i = 0; i < quantidade; i++)
+    {
         imprimirCartela(&cartelas[i], i + 1);
     }
 
